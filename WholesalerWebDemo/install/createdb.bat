7e0c62cf601724b26189d3c1cfbf573f80af9dc8
Mert Cipan <mecipan@htwg-konstanz.de>
2017-05-11 14:57:46 +0200

    Revert "Einfügen des Projekts"
    This reverts commit 8fae6dac7bcf3bd8ac3603c38c2989a5f942b6e3.

--- WholesalerWebDemo/install/createdb.bat
+++
@@ -1,3 +0,0 @@
-/usr/local/mysql/bin/mysqladmin --host=localhost --user=$2 --password=$3 drop $1 -f
-/usr/local/mysql/bin/mysqladmin --host=localhost --user=$2 --password=$3 create $1 -f
-/usr/local/mysql/bin/mysql --host=localhost --user=$2 --password=$3 --database=$1 < /Users/mertcipan/Studium/07_Semester/EBUT/02_Übung/3_Aufgabe/WholesalerWebDemo/install/ebus.sql
--- WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/ws/order/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opentrans.org/XMLSchema/1.0")
-package de.htwg_konstanz.ebus.wholesaler.ws.order;
